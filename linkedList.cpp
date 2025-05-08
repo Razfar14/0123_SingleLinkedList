f82bacb15a8b485791437017557e2b36270ddeb4
Razfar14 <rajarangkuty14@gmail.com>
2025-05-08 15:32:05 +0700

    commit Ketiga

--- linkedList.cpp
+++ linkedList.cpp
@@ -26,8 +26,30 @@ class list{
 
         if (START == NULL || nim <= START -> noMhs){
 
-            
+            if ((START != NULL)&& (nim == START ->noMhs )){
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return ;
+            }
+            node *previous = START;
+            node *current = START;
+            while ((current != NULL) && (nim >= current ->noMhs)){
+                if (nim == current ->noMhs){
+                    cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                    return ;
+                }
+                previous = current ;
+                current = current -> next;
+            }
+            nodeBaru ->next = current;
+            previous ->next = nodeBaru;
         }
+    }
+    bool listEmpty(){
+        return START == NULL;
+    }
+
+    bool search (int nim, node ** previous, node ** current){
+        
     }
 
 };
